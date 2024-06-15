63c17e3d0f990d0f86d9857c8c8a8af533a6e85b
ggrainger34 <ggrainger34@gmail.com>
2024-06-15 21:26:52 +0100

    Update gravityEngine.py

--- gravityEngine.py
+++ gravityEngine.py
@@ -19,7 +19,7 @@ screenSize = ((1000, 800))
 
 #Define computational constants as well as game constants
 #Gravitational constant can be changed
-gravitationalConstant = 10
+gravitationalConstant = 20
 eulerTimeStep = 0.01
 
 #bodyList is an array of all bodies affected by the gravitational force
@@ -27,21 +27,21 @@ bodyList = []
 
 #Define class body
 class Body:
-    def __init__(self, pos_x, pos_y, vel_x, vel_y, mass, bodyRadius, image):
+    def __init__(self, posX, posY, velX, velY, mass, bodyRadius, image):
         #Load the sprite image
         bodyImage = pygame.image.load(image)
         #Define mass, x and y postions and x and y velocities 
         #Velocities will be used to update the position of the body
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.vel_x = vel_x
-        self.vel_y = vel_y
+        self.posX = posX
+        self.posY = posY
+        self.velX = velX
+        self.velY = velY
         self.mass = mass
         self.bodyRadius = bodyRadius
 
         #Body centre, from where all calculations are carried out
-        self.bodyCentreX = self.pos_x + (self.bodyRadius / 2)
-        self.bodyCentreY = self.pos_y + (self.bodyRadius / 2)
+        self.bodyCentreX = self.posX + (self.bodyRadius / 2)
+        self.bodyCentreY = self.posY + (self.bodyRadius / 2)
         
         #The list of previousLocations is used to render the trail of the body
         self.previousLocations = []
@@ -51,9 +51,6 @@ class Body:
         bodyList.append(self)
         
     def update(self):
-        self.bodyCentreX = self.pos_x + (self.bodyRadius / 2)
-        self.bodyCentreY = self.pos_y + (self.bodyRadius / 2)
-
         #Loop through all bodies in the list and calculate how their masses impact the movement of the body
         for otherBody in bodyList:
             resultantForceX = 0
@@ -64,13 +61,13 @@ class Body:
 
             #The if statement stops bodies from being attracted to themselves 
             #If the bodies positions are the same do not count gravitational attraction. As this stops bodies from being attracted to themselves
-            if not (otherBody.pos_y == self.pos_y and otherBody.pos_x == self.pos_x):
+            if not (otherBody.posY == self.posY and otherBody.posX == self.posX):
                 #The distance between the bodies squared
-                rSquared = ((otherBody.pos_x - self.pos_x) ** 2) + ((otherBody.pos_y - self.pos_y) ** 2)
+                rSquared = ((otherBody.posX - self.posX) ** 2) + ((otherBody.posY - self.posY) ** 2)
 
                 #Find the direction in which the force is acting and normalise both x and y
-                forceDirectionX = (otherBody.bodyCentreX - self.bodyCentreX) / sqrt(rSquared)
-                forceDirectionY = (otherBody.bodyCentreY - self.bodyCentreY) / sqrt(rSquared)
+                forceDirectionX = (otherBody.posX - self.posX) / sqrt(rSquared)
+                forceDirectionY = (otherBody.posY - self.posY) / sqrt(rSquared)
 
                 #Newton's law of universal gravitation
                 singleForceMagnitude = gravitationalConstant * otherBody.mass / rSquared
@@ -86,14 +83,14 @@ class Body:
             accelerationX = resultantForceX * eulerTimeStep
             accelerationY = resultantForceY * eulerTimeStep
 
-            #Update position and velocity after calculating the desired acceleration
+            #Update position and velocity after calculating acceleration
             self.updateVelocity(accelerationX, accelerationY)
             self.updatePosition()
 
         #Update the screen
         self.renderTrail()
         self.renderBody()
-        self.previousLocations.append((self.bodyCentreX, self.bodyCentreY))
+        self.previousLocations.append((self.posX, self.posY))
 
     #This code displays where the body has been in all past positions 
     def renderTrail(self):
@@ -101,26 +98,24 @@ class Body:
             pygame.draw.circle(screen, (55,55,55), location, 1)
 
     def renderBody(self):
-        screen.blit(self.bodyImage, (self.pos_x, self.pos_y))
+        screen.blit(self.bodyImage, (self.posX, self.posY))
         
     def updateVelocity(self, accelerationX, accelerationY):
-        self.vel_x += accelerationX
-        self.vel_y += accelerationY
+        self.velX += accelerationX
+        self.velY += accelerationY
         
     def updatePosition(self):
-        self.pos_x += self.vel_x
-        self.pos_y += self.vel_y
-
-        #print(self.pos_x, self.pos_y)
+        self.posX += self.velX
+        self.posY += self.velY
         
 def makeNewBody():
     mousePos = pygame.mouse.get_pos()
-    Body(mousePos[0],mousePos[1], 0, 0, 400, 30, 'Object.png')
+    Body(mousePos[0], mousePos[1], 0, 0, 400, 30, 'Object.png')
 
 #Initialise the pygame screen and set dimensions, clock speed and title of application
 pygame.init()
 screen = pygame.display.set_mode(screenSize)
-pygame.display.set_caption('2-body Simulation')
+pygame.display.set_caption('n-body Simulation')
 clock = pygame.time.Clock()
 
 #Keep running the simulation until the user decides to quit
@@ -129,9 +124,11 @@ mouseDownLastFrame = False
 prevCounter = perf_counter()
 currentTime = perf_counter()
 
-while True:
-    #print(currentTime)
+Body(500, 500, 0, 0, 30000, 30, 'Object.png')
+Body(200, 500, 0, 2, 10, 10, 'Object.png')
+Body(800, 500, 0, -2, 10, 10, 'Object.png')
 
+while True:
     currentMouse = False
 
     for event in pygame.event.get():
@@ -145,7 +142,6 @@ while True:
         makeNewBody()
             
     if (currentTime - prevCounter) > eulerTimeStep:
-        print(currentTime - prevCounter)
         #Clear screen
         screen.fill((0,0,0))
             
